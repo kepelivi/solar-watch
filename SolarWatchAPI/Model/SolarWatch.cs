5f518773517d69725241e53194cf411f0f2402b6
kepelivi <kepelivi7@gmail.com>
2024-01-16 17:16:44 +0100

    feat - add SolarWatch class

---
+++ SolarWatchAPI/Model/SolarWatch.cs
@@ -0,0 +1,12 @@
+﻿namespace SolarWatchAPI.Model;
+
+public class SolarWatch
+{
+    public TimeOnly Sunrise;
+
+    public TimeOnly Sunset;
+
+    public DateTime Date;
+
+    public string City;
+}
