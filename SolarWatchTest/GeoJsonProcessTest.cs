401dce738def4a81d8a1b5eec84e2405148dca24
kepelivi <kepelivi7@gmail.com>
2024-01-20 13:55:53 +0100

    test - cover GeoJsonProcess with unit tests

---
+++ SolarWatchTest/GeoJsonProcessTest.cs
@@ -0,0 +1,29 @@
+﻿using SolarWatchAPI.Controllers;
+using SolarWatchAPI.Model;
+
+namespace SolarWatchTest;
+
+public class GeoJsonProcessTest
+{
+    [Test]
+    public void Should_Parse_Valid_Json_With_Lat_And_Lon_Properties()
+    {
+        string data = "[{\"lat\": 123.456, \"lon\": 789.012}]";
+
+        GeoJsonProcess geoJsonProcess = new GeoJsonProcess();
+        GeoCode result = geoJsonProcess.Process(data);
+
+        Assert.AreEqual(123.456, result.Latitude);
+        Assert.AreEqual(789.012, result.Longitude);
+    }
+
+    [Test]
+    public void Should_Throw_Key_Not_Found_Exception_When_Given_Json_Input_Without_Lat_Or_Lon_Properties()
+    {
+        string data = "[{\"lat\": 123.456}]";
+
+        GeoJsonProcess geoJsonProcess = new GeoJsonProcess();
+
+        Assert.Throws<KeyNotFoundException>(() => geoJsonProcess.Process(data));
+    }
+}
