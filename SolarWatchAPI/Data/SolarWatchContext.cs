08b2915c8edd18f380a7d5c33e4ddb8b4f656683
kepelivi <kepelivi7@gmail.com>
2024-02-02 18:08:21 +0100

    feat - integrate database with ef

---
+++ SolarWatchAPI/Data/SolarWatchContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolarWatchAPI.Model;
+
+namespace SolarWatchAPI.Data;
+
+public class SolarWatchContext : DbContext
+{
+    public DbSet<City> Cities { get; set; }
+    
+    public DbSet<Solar> Solars { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost,1433;Database=SolarWatch;User Id=sa;Password=CluelessRick2002!;Encrypt=false;");
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<City>()
+            .Property(p => p.Latitude)
+            .HasColumnType("decimal(18,12)");
+        
+        modelBuilder.Entity<City>()
+            .Property(p => p.Longitude)
+            .HasColumnType("decimal(18,13)");
+    }
+}
---
+++ SolarWatchAPI/Model/Solar.cs
@@ -0,0 +1,16 @@
+﻿namespace SolarWatchAPI.Model;
+
+public class Solar
+{
+    public int Id { get; init; }
+    public int CityId { get; init; }
+    public string Sunrise { get; init; }
+    public string Sunset { get; init; }
+
+    public Solar(int cityId, string sunrise, string sunset)
+    {
+        CityId = cityId;
+        Sunrise = sunrise;
+        Sunset = sunset;
+    }
+}
