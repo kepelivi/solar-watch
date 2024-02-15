03bbb11470314bc5f066bbc21175012c866a18bc
kepelivi <kepelivi7@gmail.com>
2024-02-15 20:55:56 +0100

    feat - add authorized endpoints for delete and update

--- SolarWatchAPI/Controllers/SolarWatchController.cs
+++ SolarWatchAPI/Controllers/SolarWatchController.cs
@@ -33,7 +33,7 @@ public class SolarWatchController : ControllerBase
         _solarJsonProcess = solarJsonProc;
     }
 
-    [HttpGet("GetSolarWatch"), Authorize]
+    [HttpGet("GetSolarWatch"), Authorize(Roles="User, Admin")]
     public async Task<ActionResult<SolarWatch>> GetSolarWatch([Required] string cityName, [Required] string date)
     {
         await using var dbContext = new SolarWatchContext();
@@ -91,6 +91,41 @@ public class SolarWatchController : ControllerBase
         {
             _logger.LogError($"Error getting solar data: {e}");
             return NotFound($"Error getting solar data: {e}");
+        }
+    }
+
+    [HttpPatch("EditSolar"), Authorize(Roles = ("Admin"))]
+    public async Task<ActionResult> EditSolar(Solar solar)
+    {
+        try
+        {
+            await using var dbContext = new SolarWatchContext();
+            dbContext.Solars.Update(solar);
+            dbContext.SaveChanges();
+            return Ok("Successfully updated sunrise and sunset time.");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError($"Error while updating solar data: {e}");
+            return NotFound($"Error while updating solar data: {e}");
+        }
+    }
+    
+    [HttpDelete("DeleteSolar"), Authorize(Roles = ("Admin"))]
+    public async Task<ActionResult> DeleteSolar([Required] int id)
+    {
+        try
+        {
+            await using var dbContext = new SolarWatchContext();
+            var solar = dbContext.Solars.Find(id);
+            dbContext.Solars.Remove(solar);
+            dbContext.SaveChanges();
+            return Ok("Successfully deleted sunrise and sunset times.");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError($"Error while deleting solar data: {e}");
+            return NotFound($"Error while deleting solar data: {e}");
         }
     }
 }
--- SolarWatchAPI/Program.cs
+++ SolarWatchAPI/Program.cs
@@ -1,8 +1,11 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SolarWatchAPI.Controllers;
 using SolarWatchAPI.Data;
+using SolarWatchAPI.Services.Authentication;
 using SolarWatchAPI.Services.SolarWatches;
 using SolarWatchAPI.Utilities;
 using ILogger = SolarWatchAPI.Utilities.ILogger;
@@ -17,7 +20,33 @@ var config = new ConfigurationBuilder()
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 builder.Services.AddSingleton<ILogger, ConsoleLogger>();
 
@@ -26,6 +55,9 @@ builder.Services.AddSingleton<IGeoJsonProcess, GeoJsonProcess>();
 
 builder.Services.AddSingleton<ISolarWatchDataProvider, SolarWatchDataProvider>();
 builder.Services.AddSingleton<ISolarJsonProcess, SolarJsonProcess>();
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 builder.Services
     .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -38,7 +70,7 @@ builder.Services
             ValidateAudience = true,
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
-            ValidIssuer = config["ValidIssuer"],
+            ValidIssuer = config["ValidateIssuer"],
             ValidAudience = config["ValidAudience"],
             IssuerSigningKey = new SymmetricSecurityKey(
                 Encoding.UTF8.GetBytes(config["IssuerSigningKey"])
@@ -47,6 +79,22 @@ builder.Services
     });
 
 builder.Services.AddDbContext<UsersContext>();
+
+// builder.Services
+//     .AddIdentityCore<IdentityUser>(options =>
+//     {
+//         options.SignIn.RequireConfirmedAccount = false;
+//         options.User.RequireUniqueEmail = true;
+//         options.Password.RequireDigit = false;
+//         options.Password.RequiredLength = 8;
+//         options.Password.RequireNonAlphanumeric = false;
+//         options.Password.RequireUppercase = true;
+//         options.Password.RequireLowercase = true;
+//     })
+//     .AddRoles<IdentityRole>()
+//     .AddEntityFrameworkStores<UsersContext>();
+
+AddIdentity();
 
 var app = builder.Build();
 
@@ -65,4 +113,71 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+
+AddRoles();
+
+AddAdmin();
+
 app.Run();
+
+void AddIdentity()
+{
+    builder.Services
+        .AddIdentityCore<IdentityUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireLowercase = true;
+        })
+        .AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<UsersContext>();
+}
+
+void AddRoles()
+{
+    using var scope = app.Services.CreateScope();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var tAdmin = CreateAdminRole(roleManager);
+    tAdmin.Wait();
+
+    var tUser = CreateUserRole(roleManager);
+    tUser.Wait();
+}
+
+async Task CreateAdminRole(RoleManager<IdentityRole> roleManager)
+{
+    await roleManager.CreateAsync(new IdentityRole(config["AdminRole"]));
+}
+
+async Task CreateUserRole(RoleManager<IdentityRole> roleManager)
+{
+    await roleManager.CreateAsync(new IdentityRole(config["UserRole"]));
+}
+
+void AddAdmin()
+{
+    var tAdmin = CreateAdminIfNotExists();
+    tAdmin.Wait();
+}
+
+async Task CreateAdminIfNotExists()
+{
+    using var scope = app.Services.CreateScope();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    var adminInDb = await userManager.FindByEmailAsync("admin@admin.com");
+    if (adminInDb == null)
+    {
+        var admin = new IdentityUser { UserName = "admin", Email = "admin@admin.com" };
+        var adminCreated = await userManager.CreateAsync(admin, config["AdminPassword"]);
+
+        if (adminCreated.Succeeded)
+        {
+            await userManager.AddToRoleAsync(admin, config["AdminRole"]);
+        }
+    }
+}
